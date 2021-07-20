d52be5463a739726ac866d417ce3bf05e65b5fc7
Rafi Barash <rafabarash@gmail.com>
2021-07-20 14:33:42 +0000

    didnt properly follow release guidelines... fixing here

--- config/const.go
+++ config/const.go
@@ -43,7 +43,7 @@ const (
 	// MinorVersion is the credential helper's minor version number.
 	MinorVersion = 0
 	// PatchVersion is the credential helper's patch version number.
-	PatchVersion = 4
+	PatchVersion = 6
 )
 
 // DefaultGCRRegistries contains the list of default registries to authenticate for.
